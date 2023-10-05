ca5cc3419b721ef7aa86d0b86bd0613aea0295c2
Volodymyr <vladikon70@gmail.com>
2023-10-05 20:11:56 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -6,8 +6,11 @@ axios.defaults.baseURL = 'https://651afec8340309952f0e22ec.mockapi.io/api/v1/';
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
+    const url = new URL(`${axios.defaults.baseURL}/contacts`);
+    url.searchParams.append('sortBy', 'name');
+    url.searchParams.append('order', 'asc');
     try {
-      const response = await axios.get('/contacts');
+      const response = await axios.get(url);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
